9032091b18498feeeccf9f5f9389a79a7d307450
Damintha Deshan Wijekoon <160304576+damintha01@users.noreply.github.com>
2025-01-22 17:21:00 +0530

    add css

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,20 +2,27 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='bg-green-800 p-9 text-white'>
-        <div className='grid grid-cols-3 gap-4'>
-            <div className='bg-green-700 p-4 rounded-lg'>
-                <h2 className='text-lg font-bold'>
-                    <p>content part 1</p>
-                </h2>
+    <div className='bg-green-800 p-9 text-white h-56 pt-5 pb-5 pl-5 pr-5'>
+        <div className='grid grid-cols-3 gap-4 h-56'>
+            <div className='bg-green-800 p-4 rounded-lg grid grid-rows-3 gap-3'>
+                <div>
+                    <p>1</p>
+                </div>
+                <div>
+                    <p>2</p>    
+                </div>    
+                <div>
+                    <p>3</p>    
+                </div>
+                
             </div>
-            <div className='bg-green-700 p-4 rounded-lg'>
-                <h2 className='text-lg font-bold'>
+            <div className='bg-green-800 p-4 rounded-lg'>
+                <h2 className='text-lg'>
                     <p>content part 2</p>
                 </h2>
             </div>
-            <div className='bg-green-700 p-4 rounded-lg'>
-                <h2 className='text-lg font-bold'>
+            <div className='bg-green-800 p-4 rounded-lg'>
+                <h2 className='text-lg'>
                     <p>content part 3</p>
                 </h2>
             </div>
