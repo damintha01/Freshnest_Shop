d74d4f842e15a49545c62465beef4dd258a76f04
Damintha Deshan Wijekoon <160304576+damintha01@users.noreply.github.com>
2025-01-22 16:49:04 +0530

    ADD

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,16 @@
 const Footer = () => {
-    return <footer>Footer Content</footer>;
+    return <footer>
+        <div>
+            <p>© 2021</p>
+        </div>
+        <div>
+            <p>Privacy Policy</p>
+        </div>
+        <div>
+            <p>Terms of Service</p>
+        </div>
+
+    </footer>;
 };
 
 export default Footer;
