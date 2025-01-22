3e0a33dc7edc862134e037a340b6bfffd5871fee
Damintha Deshan Wijekoon <160304576+damintha01@users.noreply.github.com>
2025-01-22 17:07:39 +0530

    edit footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,16 +1,30 @@
+import React from 'react'
+
 const Footer = () => {
-    return <footer>
-        <div>
-            <p>© 2021</p>
-        </div>
-        <div>
-            <p>Privacy Policy</p>
-        </div>
-        <div>
-            <p>Terms of Service</p>
+  return (
+    <div className='bg-green-800 p-9 text-white'>
+        <div className='grid grid-cols-3 gap-4'>
+            <div className='bg-green-700 p-4 rounded-lg'>
+                <h2 className='text-lg font-bold'>
+                    <p>content part 1</p>
+                </h2>
+            </div>
+            <div className='bg-green-700 p-4 rounded-lg'>
+                <h2 className='text-lg font-bold'>
+                    <p>content part 2</p>
+                </h2>
+            </div>
+            <div className='bg-green-700 p-4 rounded-lg'>
+                <h2 className='text-lg font-bold'>
+                    <p>content part 3</p>
+                </h2>
+            </div>
+
+
         </div>
 
-    </footer>;
-};
+    </div>
+  )
+}
 
-export default Footer;
+export default Footer
