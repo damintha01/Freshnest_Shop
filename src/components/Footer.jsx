0c5ff6493896b42ae46f97ab3218c394797cfbe0
Damintha Deshan Wijekoon <160304576+damintha01@users.noreply.github.com>
2025-01-24 21:02:57 +0530

    1/24 e2

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,73 +1,83 @@
-import React from 'react';
+import React from 'react'
 import { FaFacebook } from "react-icons/fa";
 import { FaTwitter } from "react-icons/fa";
 import { FaInstagram } from "react-icons/fa";
 import { FaLinkedin } from "react-icons/fa";
+import { FaCcVisa } from "react-icons/fa";
+import { FaCcMastercard } from "react-icons/fa";
+import { SiAmericanexpress } from "react-icons/si";
+import { SiGooglepay } from "react-icons/si";
 
 const Footer = () => {
   return (
     <div className=''>
-      {/* Green Section */}
-      <div className='bg-green-800 text-white h-56'>
-        <div className='grid grid-cols-4 gap-4'>
-          {/* Useful Links */}
-          <div className='bg-green-800 p-4 rounded-lg grid grid-rows-3 gap-3 text-left'>
-            <h2>
-              <p className='text-lg text-black'>Useful Links</p>
-            </h2>
-            <div>
-              <p>Privacy Policy</p>
+    
+    <div className='bg-green-800 text-white h-56 p-1'>
+        <div className='grid grid-cols-4 gap-4 h-20'>
+            <div className='bg-green-800 p-4 rounded-lg grid grid-rows-3 gap-3 text-left pl-24'>
+                <h2><p className='text-lg text-black'>Useful Links</p></h2>
+                <div>
+                    <p>Privacy Policy</p>
+                </div>
+                <div>
+                    <p>FAQ</p>    
+                </div>    
+                <div>
+                    <p>Terms and Conditions</p>    
+                </div>
+                <div>
+                    <p>Stores</p>    
+                </div>
+                
             </div>
-            <div>
-              <p>FAQ</p>
+            <div className='bg-green-800 p-4 rounded-lg pl-10'>
+                <h2 className='text-left'>
+                    <p className='text-lg text-black'>Customer Service</p>
+                    <p>Contact us</p>
+                    <p>About us</p>
+                </h2>
             </div>
-            <div>
-              <p>Terms and Conditions</p>
+            <div className='bg-green-800 p-4 rounded-lg pr-12'>
+                <div className='grid grid-cols-4 text-3xl '>
+                    <div >
+                        <FaFacebook/>
+                    </div>
+                    <div>
+                        <FaTwitter/>
+                    </div>
+                    <div>
+                        <FaInstagram/>
+                    </div>
+                    <div>
+                        <FaLinkedin/>
+                    </div>
+                </div>
             </div>
-            <div>
-              <p>Stores</p>
+            <div className='bg-green-800 p-4 rounded-lg'>
+                <h2 className='text-lg'>
+                    <p>content part 4</p>
+                </h2>
             </div>
-          </div>
-          {/* Customer Service */}
-          <div className='bg-green-800 p-4 rounded-lg'>
-            <h2 className='text-left'>
-              <p className='text-lg text-black'>Customer Service</p>
-              <p>Contact us</p>
-              <p>About us</p>
-            </h2>
-          </div>
-          {/* Social Media Icons */}
-          <div className='bg-green-800 p-4 rounded-lg'>
-            <div className='grid grid-cols-4 text-3xl '>
-              <div>
-                <FaFacebook />
-              </div>
-              <div>
-                <FaTwitter />
-              </div>
-              <div>
-                <FaInstagram />
-              </div>
-              <div>
-                <FaLinkedin />
-              </div>
-            </div>
-          </div>
-          {/* Content Part 4 */}
-          <div className='bg-green-800 p-4 rounded-lg'>
-            <h2 className='text-lg'>
-              <p>content part 4</p>
-            </h2>
-          </div>
+
+
         </div>
-      </div>
 
-      {/* Black Footer Section */}
-      <div className='bg-black text-white h-12 flex items-center justify-center'>
-        <p className='text-center'>© 2021 All rights reserved</p>
-      </div>
     </div>
-  );
-};
 
-export default Footer;
+    <div>
+        <div className='bg-black text-white h-20 m-0 content-center grid grid-cols-2 gap-6'>    
+            <p className='text-center'>© 2021 All rights reserved</p>
+            <div className='grid grid-cols-4 text-4xl'>
+                <FaCcVisa/>
+                <FaCcMastercard/>
+                <SiAmericanexpress/>
+                <SiGooglepay/>
+
+            </div>
+        </div>
+    </div>
+    </div>
+  )
+}
+
+export default Footer
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,7 +9,7 @@ const Home = () => {
         <div className='min-h-screen bg-gray-200 text-center p-5'>
             <h1>Home</h1>
         </div>
-        <div className='bg-green-800 text-white text-center p-9'>
+        <div className=''>
             <Footer />
         </div>
     </div>
