34ecedc2718e521b8605c36dc1e0dc2fca1b64f9
Damintha Deshan Wijekoon <160304576+damintha01@users.noreply.github.com>
2025-01-26 01:26:41 +0530

    1/26 e3

--- src/components/Menulink.jsx
+++ src/components/Menulink.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Menulink = (props) => {
   return (
     <div>
-        <a href={props.url} className='text-white hover:text-gray-900 text-lg'>{props.linkname}</a>
+        <a href={props.url} className='text-black hover:text-green-700 text-lg'>{props.linkname}</a>
     </div>
   )
 }
