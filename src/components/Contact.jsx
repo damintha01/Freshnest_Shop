1fc36ed6b50026898048882b5417f81155884f9a
Damintha Deshan Wijekoon <160304576+damintha01@users.noreply.github.com>
2025-01-26 23:25:46 +0530

    create page bg

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,17 +1,20 @@
 import React from 'react'
 import Navbar from './Navbar'
+import bg from '../Img/bg.webp'
+import Footer from './Footer'
 
 const Contact = () => {
   return (
-    <div>
+    <div className='bg-cover bg-center opacity-100 w-full' style={{backgroundImage: `url(${bg})`}}>
         <Navbar />
-        <div>
+        <div className='min-h-screen p-5'>
             <h1>Contact</h1>
             <h1>About</h1>
             <h1>gggggggggg</h1>
             <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta, esse optio mollitia sit quae adipisci voluptas, eius at fuga assumenda dolores. Optio, officiis ex quibusdam ipsa maiores perferendis laboriosam? Temporibus!</p>
         
         </div>
+        <Footer />
     </div>
   )
 }
