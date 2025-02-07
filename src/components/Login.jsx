8c07c0b1d0c6c9a717f9f00d55016109462c654c
Damintha Deshan Wijekoon <160304576+damintha01@users.noreply.github.com>
2025-02-07 15:59:03 +0530

    a

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -8,7 +8,7 @@ const Login = () => {
     <div className='bg-cover bg-center opacity-100' style={{backgroundImage: `url(${bg})`}}>
         <Navbar />
         <div className='min-h-screen p-5'>
-            <h1>Login...a.</h1>
+            <h1>Login...</h1>
         </div>
         <Footer />
     </div>
