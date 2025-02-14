c3edd1234f2abfc088f4a7ccc9e07c46625d4681
Damintha Deshan Wijekoon <160304576+damintha01@users.noreply.github.com>
2025-02-14 10:35:47 +0530

    2142

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -8,7 +8,7 @@ const News = () => {
     <div className='bg-cover bg-center opacity-100' style={{backgroundImage: `url(${bg})`}}>
         <Navbar />
         <div className='min-h-screen p-5'>
-            <h1>News</h1>
+            <h1>News..</h1>
         </div>
         <Footer />
     </div>
