5eb9702bc51e907132210b6c1f336190c2897b73
Damintha Deshan Wijekoon <160304576+damintha01@users.noreply.github.com>
2025-03-17 22:29:18 +0530

    add

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import Navbar from './Navbar'
 import Footer from '../components/Footer'
+import ASHPLANTAIN from '../Img/vegitable/ASH PLANTAIN.avif'
+import BEANS from '../Img/vegitable/BEANS.avif'
+import Bigonion from '../Img/vegitable/BIG ONION.avif'
+import Potato from '../Img/vegitable/POTATO.avif'
 
 
 const Home = () => {
@@ -8,7 +12,15 @@ const Home = () => {
     <div >
         <Navbar />
         <div className='min-h-screen text-center p5'>
-            <h1>Home.....</h1>
+          <div className='text-4xl font-bold text-green-500 mt-10'>Welcome to our online market</div>
+          <div className='text-xl font-bold text-green-500'>We sell fresh vegetables</div>
+          <div className='m-2 w-60 flex float-left space-x-9'>
+            <img className='' src={ASHPLANTAIN} alt="" />
+            <img className=' ' src={BEANS} alt="" />
+            <img className=' ' src={Bigonion} alt="" />
+            <img className=' ' src={Potato} alt="" />
+          </div>
+
         </div>
         <div className=''>
             <Footer />
